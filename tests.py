4d7cb213e7efd698f5dd8ce8b5e7f1e15157c69c
laskovatr <65371301+laskovatr@users.noreply.github.com>
2022-05-03 21:24:30 +0300

    Add files via upload

--- tests.py
+++ tests.py
@@ -2,5 +2,71 @@ from Objects import *
 from Mark import *
 
 
+def MobTest():
+    print("OK")
+    return True
 
 
+
+
+def MobBonusTest():
+    print("OK")
+    return True
+
+
+
+
+def MobGumbaTest():
+    print("OK")
+    return True
+
+
+
+
+def MobMushroomTest():
+    print("OK")
+    return True
+
+
+
+
+def MobTests():
+    if (not MobTest()):
+        print("MobTestError")
+        return False
+    if (not MobBonusTest()):
+        print("MobBonusTestError")
+        return False
+    if (not MobGumbaTest()):
+        print("MobGumbaTestError")
+        return False
+    if (not MobMushroomTest()):
+        print("MobMushroomTestError")
+        return False
+    print("OK")
+    return True
+
+
+
+
+def MarkTests():
+    print("OK")
+    return True
+
+
+
+
+def Tests():
+    if (not MobTests()):
+        print("MobTestsError")
+        return False
+    if (not MarkTests()):
+        print("MarkTestsError")
+        return False
+    print("OK")
+    return True
+
+
+
+
+Tests()
